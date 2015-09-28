c7c76e88ec46121eaeb6a30f20755ffb073c5948
GitScrum <Scrum@list.ru>
2015-09-28 22:27:02 +0300

    update build system

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,7 @@ var gulp = require('gulp'),
 	postcss = require('gulp-postcss'),
     rename = require('gulp-rename'),
     ghPages = require('gulp-gh-pages'),
-	shell = require('gulp-shell'),
-	changelog = require('gulp-conventional-changelog');
+	shell = require('gulp-shell')
 
 	require('gulp-release-tasks')(gulp);
 
@@ -18,29 +17,14 @@ var pkg = require('./package.json'),
 		'*'
 		].join('\n');
 
-gulp.task('changelog', function() {
-    return gulp.src('CHANGELOG.md', {
-        buffer: false
-    })
-    .pipe(changelog({
-      preset: 'angular'
-    }))
-    .pipe(gulp.dest('./'));
-});
-
-gulp.task('copy', function() {
-    return gulp.src('./dist/**/*.*', {base: './dist/'})
-      .pipe(gulp.dest('./docs/dist/'))
-});
-
 gulp.task('jekyll', function() {
     return gulp.src('*.js', {read: false})
       .pipe(shell('jekyll build'))
 });
 
-gulp.task('deploy', ['pcss', 'copy', 'jekyll'] ,function() {
+gulp.task('deploy', ['pcss', 'jekyll'] ,function() {
     return gulp.src('./_gh-pages/**/*')
-      .pipe(ghPages())
+      //.pipe(ghPages())
 });
 
 gulp.task('pcss', function() {
@@ -56,7 +40,8 @@ gulp.task('pcss', function() {
     		require('postcss-banner')({banner: slim_banner})
     	]))
     	.pipe(rename({ extname: '.css' }))
-    	.pipe(gulp.dest('./dist/css/'));
+    	.pipe(gulp.dest('./dist/css/'))
+        .pipe(gulp.dest('./docs/dist/'))
 });
 
 
