b800c20cc77002f86e92bec7b808b18de176eaf2
Ivan Demidov <demidov@220-volt.ru>
2015-09-28 18:04:28 +0300

    update builder system

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp'),
 	postcss = require('gulp-postcss'),
-	rename = require('gulp-rename'),
+    rename = require('gulp-rename'),
+    ghPages = require('gulp-gh-pages'),
+	shell = require('gulp-shell'),
 	changelog = require('gulp-conventional-changelog');
 
 	require('gulp-release-tasks')(gulp);
@@ -26,9 +28,19 @@ gulp.task('changelog', function() {
     .pipe(gulp.dest('./'));
 });
 
-gulp.task('docs',['pcss'], function() {
+gulp.task('copy', function() {
     return gulp.src('./dist/**/*.*', {base: './dist/'})
-      .pipe(gulp.dest('./docs/dist/'));
+      .pipe(gulp.dest('./docs/dist/'))
+});
+
+gulp.task('jekyll', function() {
+    return gulp.src('*.js', {read: false})
+      .pipe(shell('jekyll build'))
+});
+
+gulp.task('deploy', ['pcss', 'copy', 'jekyll'] ,function() {
+    return gulp.src('./_gh-pages/**/*')
+      .pipe(ghPages())
 });
 
 gulp.task('pcss', function() {
