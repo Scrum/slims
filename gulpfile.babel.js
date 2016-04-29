2d564e40e1dd005c20dc4951e15c248018fa3d89
Ivan Demidov <demidov@220-volt.ru>
2016-04-29 16:17:36 +0300

    Fixed #111, change gulp shell to execa

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,7 +3,7 @@ import postcss from 'gulp-postcss';
 import rename from 'gulp-rename';
 import ghPages from 'gulp-gh-pages';
 import pkg from './package.json';
-import shell from 'gulp-shell';
+import execa from 'execa';
 import cssValidator from 'gulp-css-validator';
 
 const slim_banner = (
@@ -16,8 +16,7 @@ const slim_banner = (
 	*`);
 
 export function jekyll() {
-	return gulp.src('*.js', {read: false})
-		.pipe(shell('jekyll build'))
+	return execa.shell('jekyll build');
 }
 
 export function deployGhPages() {
