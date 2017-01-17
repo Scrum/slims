bd305fe7151c9904ffebbdc2c3987e26143de6d9
Ivan Demidov <scrum@list.ru>
2017-01-17 12:07:16 +0300

    docs(*): remove gulp-gh-pages

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,16 +1,5 @@
 import gulp from 'gulp';
 import postcss from 'gulp-postcss';
-import ghPages from 'gulp-gh-pages';
-import execa from 'execa';
-
-export function jekyll() {
-	return execa.shell('jekyll build');
-}
-
-export function deployGhPages() {
-	return gulp.src('./_gh-pages/**/*')
-		.pipe(ghPages());
-}
 
 export function csssupport() {
 	return gulp.src('./dist/css/**/*.css')
@@ -26,8 +15,5 @@ export function csssupport() {
 
 const test = gulp.parallel(csssupport);
 export {test};
-
-const deploy = gulp.series(jekyll, deployGhPages);
-export {deploy};
 
 export default test;
