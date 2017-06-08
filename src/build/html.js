5fa8fe2c87b3c76ff1c3dfb821671aeb5fb5d692
Ivan Demidov <demidov@220-volt.ru>
2017-06-08 11:05:02 +0300

    chore: update changelog

--- src/build/html.js
+++ src/build/html.js
@@ -5,25 +5,22 @@ const posthtml = require('posthtml');
 [
 	'./src/docs/index.html',
 	'./src/docs/grid-col.html',
-	'./src/docs/grid-row.html',
+	'./src/docs/grid-row.html'
 ].forEach(file => {
 	const html = fs.readFileSync(path.resolve(file), 'utf8');
 
 	posthtml([
-			require('posthtml-extend')({
-			  root: 'src/docs/'
-			}),
-			require('posthtml-modules')({
-			  root: 'src/docs/',
-			  from: 'src/docs/layout/'
-			}),
-			require('posthtml-spaceless')()
-		])
-		.process(html)
-		.then(result => {
-			fs.writeFileSync(path.resolve(`./docs/${path.basename(file)}`), result.html);
-		});
+		require('posthtml-extend')({
+			root: 'src/docs/'
+		}),
+		require('posthtml-modules')({
+			root: 'src/docs/',
+			from: 'src/docs/layout/'
+		}),
+		require('posthtml-spaceless')()
+	])
+	.process(html)
+	.then(result => {
+		fs.writeFileSync(path.resolve(`./docs/${path.basename(file)}`), result.html);
+	});
 });
-
-
-
