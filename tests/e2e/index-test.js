19d06640b5d051e86aa17eea9f23e5905295ca51
Ivan Demidov <demidov@220-volt.ru>
2017-06-08 10:21:28 +0300

    style: indent

--- tests/e2e/index-test.js
+++ tests/e2e/index-test.js
@@ -1,26 +1,26 @@
-import { IndexPage, GridColPage, GridRowPage } from './page-models';
+import {IndexPage, GridColPage, GridRowPage} from './page-models';
 
-fixture `Initial tests`;
+fixture`Initial tests`;
 
 test('check element existence', async t => {
-    const indexPage = new IndexPage();
+	const indexPage = new IndexPage();
 
-    await t.navigateTo(indexPage.url)
-        .expect(indexPage.gridColLink.exists).ok()
-        .expect(indexPage.gridRowLink.exists).ok()
-        .expect(indexPage.mainContainer.exists).ok();
+	await t.navigateTo(indexPage.url)
+		.expect(indexPage.gridColLink.exists).ok()
+		.expect(indexPage.gridRowLink.exists).ok()
+		.expect(indexPage.mainContainer.exists).ok();
 });
 
 test('check block sizes', async t => {
-    const gridColPage = new GridColPage();
-    const gridRowPage = new GridRowPage();
+	const gridColPage = new GridColPage();
+	const gridRowPage = new GridRowPage();
 
-    await t.navigateTo(gridColPage.url)
-        .expect(gridColPage.colums.count).eql(12)
-        .resizeWindow(500, 500)
-        .expect(gridColPage.colums.nth(0).clientWidth).eql(29)
-        .navigateTo(gridRowPage.url)
-        .expect(gridRowPage.rows.count).eql(12)
-        .resizeWindow(500, 500)
-        .expect(gridRowPage.rows.clientHeight).eql(25);
+	await t.navigateTo(gridColPage.url)
+		.expect(gridColPage.colums.count).eql(12)
+		.resizeWindow(500, 500)
+		.expect(gridColPage.colums.nth(0).clientWidth).eql(29)
+		.navigateTo(gridRowPage.url)
+		.expect(gridRowPage.rows.count).eql(12)
+		.resizeWindow(500, 500)
+		.expect(gridRowPage.rows.clientHeight).eql(25);
 });
